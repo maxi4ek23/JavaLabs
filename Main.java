2ce1959cbab9ba845ef9817b2aaf76f41d5ea327
Максим Пелишко <91155188+maxi4ek23@users.noreply.github.com>
2022-02-23 21:06:21 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ package ua.lviv.iot.firstLab;
 public class Main {
 
 	public static void main(String[] args) {
-		Farm CowFarm = new Farm();
-		Farm PigFarm = new Farm("Kyiv", 50, 6);
-		Farm FishFarm = new Farm("Harkiv", 65, 7, 120, "Bogdan", true);
-        System.out.println(CowFarm);
-        System.out.println(PigFarm);
-        System.out.println(FishFarm);
+		Farm cowFarm = new Farm();
+		Farm pigFarm = new Farm("Kyiv", 50, 6);
+		Farm fishFarm = new Farm("Harkiv", 65, 7, 120, "Bogdan", true);
+        System.out.println(cowFarm);
+        System.out.println(pigFarm);
+        System.out.println(fishFarm);
         
         
 	}
